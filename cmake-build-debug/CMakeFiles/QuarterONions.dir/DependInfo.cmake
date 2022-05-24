ce773b2e995a868c982e03a551857dadb5e54cb6
Pierre Durand <pierre.durand95360@gmail.com>
2022-05-24 19:10:11 +0200

    yeet

---
+++ cmake-build-debug/CMakeFiles/QuarterONions.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/pierr/Documents/GitHub/QuarterONions/Quateronion.cpp" "CMakeFiles/QuarterONions.dir/Quateronion.cpp.obj" "gcc" "CMakeFiles/QuarterONions.dir/Quateronion.cpp.obj.d"
+  "C:/Users/pierr/Documents/GitHub/QuarterONions/Vector3.cpp" "CMakeFiles/QuarterONions.dir/Vector3.cpp.obj" "gcc" "CMakeFiles/QuarterONions.dir/Vector3.cpp.obj.d"
+  "C:/Users/pierr/Documents/GitHub/QuarterONions/common/GLShader.cpp" "CMakeFiles/QuarterONions.dir/common/GLShader.cpp.obj" "gcc" "CMakeFiles/QuarterONions.dir/common/GLShader.cpp.obj.d"
+  "C:/Users/pierr/Documents/GitHub/QuarterONions/main.cpp" "CMakeFiles/QuarterONions.dir/main.cpp.obj" "gcc" "CMakeFiles/QuarterONions.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/QuarterONions.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QuarterONions.dir/Quateronion.cpp.obj"
+  "CMakeFiles/QuarterONions.dir/Quateronion.cpp.obj.d"
+  "CMakeFiles/QuarterONions.dir/Vector3.cpp.obj"
+  "CMakeFiles/QuarterONions.dir/Vector3.cpp.obj.d"
+  "CMakeFiles/QuarterONions.dir/common/GLShader.cpp.obj"
+  "CMakeFiles/QuarterONions.dir/common/GLShader.cpp.obj.d"
+  "CMakeFiles/QuarterONions.dir/main.cpp.obj"
+  "CMakeFiles/QuarterONions.dir/main.cpp.obj.d"
+  "QuarterONions.exe"
+  "QuarterONions.exe.manifest"
+  "QuarterONions.pdb"
+  "libQuarterONions.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QuarterONions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
